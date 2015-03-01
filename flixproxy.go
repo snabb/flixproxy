bf7016d6d86712bad0e103e76d1af4cf23ca04be
Janne Snabb <snabb@epipe.com>
2015-03-02 00:11:57 +0200

    Fix typo.

--- flixproxy.go
+++ flixproxy.go
@@ -160,7 +160,7 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with flixproxy. If not, see <http://www.gnu.org/licenses/>.
+along with Flixproxy. If not, see <http://www.gnu.org/licenses/>.
 `)
 }
 
